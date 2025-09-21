c8d4ab617eb683985ba6bc8d6465a1fa9bc6b268
Gart <gmhobson@gmail.com>
2025-09-21 15:15:37 -0500

    fixed right turn transition bug

--- src/Entities/Actors/Actions/turn_action.gd
+++ src/Entities/Actors/Actions/turn_action.gd
@@ -30,7 +30,7 @@ func perform(game: Game, entity: Entity) -> void:
 			playerFacing = game.directions[nextDirection]
 			
 	if direction == "Right":
-		transitions.play("")
+		transitions.play("Right")
 		if game.playerFacing == game.directions.WEST:
 			#easy solution, just loop back to other side of the dictionary
 			playerFacing = game.directions.NORTH
